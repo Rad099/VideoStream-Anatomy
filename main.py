5e74a433a6c45a68e6237776862893a8a40e15c7
Ridwan Alrefai <malref2@uic.edu>
2023-09-10 22:23:43 -0500

    Setup files

---
+++ main.py
@@ -0,0 +1,31 @@
+'''
+Ridwan Alrefai - Matteo
+University of Illinois Chicago
+main.py
+   
+   
+The main file has the main 4 steps of the project, and at each step, presents the spatial and frequency data
+   
+   
+'''
+
+import util
+
+
+# Original RGB components
+
+
+
+# Orgiinal YIQ components
+
+
+
+# Composite signal
+
+
+
+# Recovered YIQ
+
+
+
+# Recovered RGB
